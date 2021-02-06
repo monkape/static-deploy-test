0bfef3283a905b7061f1276454770b7e048c3ad2
Jan Ivica <wesleymustdie@gmail.com>
2021-02-06 13:46:49 +0100

    2021-02-06T12:46:47.115Z

---
+++ client/api/map.7MIhyaVv8t7yWHfFWBCt-PgbrQj2O028JtAbxb1EsNI.js
@@ -0,0 +1,3 @@
+window.api_map = {
+  "https://cdn.contentful.com/spaces/4e6wl1dk7dpi/entries?access_token=962gEVQev2FBml8IAi-nXXIyetxMuJAgZtPN7UF2JEs": "\\static-deploy-test\\client\\api\\Qxj6BwD1n-bjKjC3SLHY_AxyU_o2cFPiAgtBypziPxw.json"
+}
