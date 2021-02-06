85e4e9db83204643551942c616042823977db929
Jan Ivica <wesleymustdie@gmail.com>
2021-02-06 18:26:13 +0100

    2021-02-06T17:26:11.235Z

--- client/api/map.7MIhyaVv8t7yWHfFWBCt-PgbrQj2O028JtAbxb1EsNI.js
+++
@@ -1,3 +0,0 @@
-window.api_map = {
-  "https://cdn.contentful.com/spaces/4e6wl1dk7dpi/entries?access_token=962gEVQev2FBml8IAi-nXXIyetxMuJAgZtPN7UF2JEs": "\\static-deploy-test\\client\\api\\Qxj6BwD1n-bjKjC3SLHY_AxyU_o2cFPiAgtBypziPxw.json"
-}
---
+++ client/api/map.msVqUItRWej7OEZppCfNBY9Zgv-SGY3wkhwLVoCSuXQ.js
@@ -0,0 +1,3 @@
+window.api_map = {
+  "https://cdn.contentful.com/spaces/4e6wl1dk7dpi/entries?access_token=962gEVQev2FBml8IAi-nXXIyetxMuJAgZtPN7UF2JEs": "\\static-deploy-test\\client\\api\\5RdQCpz6r5tzHwzWxQgdO2m1RFDQurZqOPky1qEs9Mw.json"
+}
